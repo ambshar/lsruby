24b74b0f2a3951dfe80199ef53589dc82631a255
azs <ambshar@gmail.com>
2016-05-11 13:21:37 -0700

    changes to rock paper scissors after CR

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,41 +1,32 @@
+require 'pry'
 VALID_CHOICES = %w(rock paper scissors spock lizard)
-VALID_SHORT_FORMS = %w(r p sc s l)
+VALID_SHORT_FORMS = {
+  "r" => "rock",
+  "p" => "paper",
+  "sc" => "scissors",
+  "s" => "spock",
+  "l" => "lizard"
+}
+
+ENCOUNTER_RULES = {
+  rock: [:scissors, :lizard],
+  paper: [:rock, :spock],
+  scissors: [:paper, :lizard],
+  lizard: [:spock, :paper],
+  spock: [:scissors, :rock]
+}
 
 def prompt(message)
   puts("=> #{message}")
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper') ||
-    (first == 'rock' && second == 'lizard') ||
-    (first == 'lizard' && second == 'spock') ||
-    (first == 'spock' && second == 'scissors') ||
-    (first == 'scissors' && second == 'lizard') ||
-    (first == 'lizard' && second == 'paper') ||
-    (first == 'paper' && second == 'spock') ||
-    (first == 'spock' && second == 'rock')
-end
-
-def expand_short_form(choice)
-  case choice
-  when 'r'
-    'rock'
-  when 'p'
-    'paper'
-  when 'l'
-    'lizard'
-  when 'sc'
-    'scissors'
-  when 's'
-    'spock'
-  end
+  ENCOUNTER_RULES[first.to_sym].include? second.to_sym
 end
 
 def display_result(player, computer)
   if win?(player, computer)
-    prompt("You won!")
+    prompt("YOU WON!")
   elsif win?(computer, player)
     prompt("Computer won!")
   else
@@ -52,15 +43,15 @@ loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     prompt("You can type r for rock or p for paper and so on.  Type sc for scissors")
     choice = Kernel.gets().chomp()
+    choice = VALID_SHORT_FORMS[choice] if VALID_SHORT_FORMS.key? choice
+
     if VALID_CHOICES.include?(choice)
-      break
-    elsif VALID_SHORT_FORMS.include?(choice)
-      choice = expand_short_form(choice)
       break
     else
       prompt("This is not a valid choice")
     end
   end # end choice input loop
+
   computer_choice = VALID_CHOICES.sample
 
   display_result(choice, computer_choice)
@@ -72,13 +63,10 @@ loop do
   end
   prompt("Your score: #{player_win_count}.  Computer score: #{computer_win_count}")
   if player_win_count == 5
-    prompt("You are the first to 5 wins.  You win the game")
+    prompt("You are the first to 5 wins.  YOU WIN THE GAME")
     break
   elsif computer_win_count == 5
     prompt("Computer wins.  Bye.")
     break
   end
-  prompt("Do you want to play again?")
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase.start_with?('y')
 end
